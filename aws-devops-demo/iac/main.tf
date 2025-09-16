f294d3bde8cc2ea9c91c9241aadefca81d474c70
Chiranjeevi-AR <chiranjeeviar.83@gmail.com>
2025-09-17 00:29:32 +0530

    iac: pin subnet to ap-south-1a to support t2.micro

--- aws-devops-demo/iac/main.tf
+++ aws-devops-demo/iac/main.tf
@@ -30,6 +30,7 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
 
   tags = { Name = "aws-devops-demo-public-a" }
 }
